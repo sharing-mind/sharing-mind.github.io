fe3ad466cc94b28a8aadd2cdf0f1cd5054f8313e
Biplob <biplob.ruet15@gmail.com>
2022-06-02 00:33:12 +0600

    integrate disqus

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,15 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import DisqusEmbaded from "../../src/components/DisqusEmbaded";
 
 export default function Posts() {
   return (
-    <div>Posts Page 
-    <Link href='/'>Home Page</Link></div>
-  )
+    <div>
+      Posts Page
+      <Link href="/">Home Page</Link>
+      <div style={{ margin: "50px 300px" }}>
+        <DisqusEmbaded />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/DisqusEmbaded.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { DiscussionEmbed } from "disqus-react";
+
+export default function DisqusEmbaded() {
+  return <DiscussionEmbed shortname="biplob-2-disqus-com" config={{}} />;
+}
